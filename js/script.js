fe4348476428bf00a2fdf88b020c6b2038a36ac7
Jhojan Mendoza Bautista <Jhojanbautista11@gmail.com>
2024-10-16 15:15:50 -0500

    Rediseño

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 // Define los enlaces a las redes sociales
 const socialLinks = {
-    whatsapp: "https://wa.me/3212692311"
+    whatsapp: "https://wa.me/3212692311",
+    instagram: "https://www.instagram.com/jhojan_mendoza_16/",
+    github: "https://github.com/JhojanSweetHeart"
 };
 
 document.getElementById("whatsapp").addEventListener("click", function() {
     window.open(socialLinks.whatsapp, "_blank");
+});
+
+document.getElementById("github").addEventListener("click", function() {
+    window.open(socialLinks.github, "_blank");
+});
+
+document.getElementById("instagram").addEventListener("click", function() {
+    window.open(socialLinks.instagram, "_blank");
 });
