e45bf08871bec8fe80547ad2040edcc31f90934a
Jhojan <Jhojanbautista11@gmail.com>
2024-11-24 14:49:49 -0500

    Indexacion con motores de busqueda

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Define los enlaces a las redes sociales
 const socialLinks = {
-    whatsapp: "https://wa.me/3212692311",
+    whatsapp: "https://wa.me/3025934518",
     instagram: "https://www.instagram.com/jhojan_mendoza_16/",
     github: "https://github.com/JhojanSweetHeart"
 };
@@ -16,3 +16,65 @@ document.getElementById("github").addEventListener("click", function() {
 document.getElementById("instagram").addEventListener("click", function() {
     window.open(socialLinks.instagram, "_blank");
 });
+
+// Inicialización de Particles.js
+particlesJS("particles-js", {
+    particles: {
+      number: {
+        value: 310,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+      },
+      color: {
+        value: "#fff",
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#000000",
+        },
+        polygon: {
+          nb_sides: 5,
+        },
+      },
+      opacity: {
+        value: 1,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false,
+        },
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: {
+          enable: false,
+        },
+      },
+      line_linked: {
+        enable: false,
+      },
+      move: {
+        enable: true,
+        speed: 2,
+        direction: "bottom",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200,
+        },
+      },
+    },
+    retina_detect: true,
+  });
+  
